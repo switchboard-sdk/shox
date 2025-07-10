16c96a2f611b2e9d09c7ebed6b1591230aa46b7e
Ivan Nador <github@ivannador.com>
2025-07-10 16:59:33 +0200

    Added invoke tasks

---
+++ tasks.py
@@ -0,0 +1,24 @@
+
+from invoke import task, Collection
+import shutil
+
+@task
+def lint(c):
+    """Lint the source using clang-format"""
+    # Check if clang-format is installed
+    if not shutil.which("clang-format"):
+        print("Error: clang-format is not installed or not in PATH")
+        return
+    # Recursively lint include/shox directory
+    c.run("find include/shox -name '*.h' -o -name '*.hpp' -o -name '*.cpp' -o -name '*.cc' | xargs clang-format -i")
+    print("Linting completed")
+
+@task
+def build_and_package(c):
+    """Build, install and package the project"""
+    c.run("cmake -G 'Ninja Multi-Config' -S . -B build/manual -DSHOX_ENABLE_TESTING=OFF")
+    c.run("cmake --build build/manual --config Release -j 8")
+    with c.cd("build/manual"):
+        c.run("cpack")
+        c.run("mkdir -p ../../out")
+        c.run("cp shox-* ../../out/")
